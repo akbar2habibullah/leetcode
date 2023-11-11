3fd8d2e0317ca031746358296e1a51fb2dc79e46
Habibullah Akbar <akbar2habibullah@gmail.com>
2023-11-11 17:13:55 +0700

    Time: 824 ms (20.13%), Space: 0B (100%) - LeetHub

---
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+SELECT 
+    CASE
+        WHEN MOD(id, 2) != 0 AND id = counts THEN id
+        WHEN MOD(id, 2) != 0 AND id != counts THEN id + 1
+        ELSE id - 1
+    END AS id,
+    student
+FROM 
+    seat,
+    (SELECT 
+        COUNT(*) AS counts 
+    FROM 
+        seat) AS seat_counts
+ORDER BY id ASC;
