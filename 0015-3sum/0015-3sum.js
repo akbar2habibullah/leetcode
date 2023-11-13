d9b3f9498bdff2171c77c5b34ec5dbad9735246c
Habibullah Akbar <akbar2habibullah@gmail.com>
2023-11-13 13:26:40 +0700

    Time: 148 ms (73.37%), Space: 59 MB (59.34%) - LeetHub

---
+++ 0015-3sum/0015-3sum.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    nums.sort((a, b) => a - b);
+    const result = [];
+    for (let i = 0; i < nums.length - 2; i++) {
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+        let j = i + 1;
+        let k = nums.length - 1;
+        while (j < k) {
+            if (nums[i] + nums[j] + nums[k] === 0) {
+                result.push([nums[i], nums[j], nums[k]]);
+                j++;
+                while (j < k && nums[j] === nums[j - 1]) j++;
+                k--;
+                while (j < k && nums[k] === nums[k + 1]) k--;
+            } else if (nums[i] + nums[j] + nums[k] < 0) {
+                j++;
+            } else {
+                k--;
+            }
+        }
+    }
+    return result;
+};
